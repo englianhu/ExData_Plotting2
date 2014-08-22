9a28670cb7905853cf805a78454e8c7696908e09
Eng Lian Hu <englianhu@users.noreply.github.com>
2014-08-23 00:58:06 +0800

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,24 +1,23 @@
-# Exploratory Data Analysis - Assignment 2 - Q. #2
-# Ron Mashrouteh May 23, 2014
+## Download raw zipped data
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+destfile <- "NEI_data.zip"
+download.file(fileUrl, destfile=paste("data", destfile, sep="/"))
 
-# Loading provided datasets - loading from local machine
-NEI <- readRDS("~/Exploratory_Data_Analysis/Assignment_2/summarySCC_PM25.rds")
-SCC <- readRDS("~/Exploratory_Data_Analysis/Assignment_2/Source_Classification_Code.rds")
+## Unzip the dataset
+unzip(paste("data", destfile, sep="/"), exdir="data")
+data_dir <- setdiff(dir("data"), destfile)
 
-# Sampling
-NEI_sampling <- NEI[sample(nrow(NEI), size=5000, replace=F), ]
+# Read dataset
+NEI <- readRDS("./data/summarySCC_PM25.rds")
+SCC <- readRDS("./data/Source_Classification_Code.rds")
 
-# Subset data and append two years in one data frame
+# Subset data
 MD <- subset(NEI, fips=='24510')
 
-# Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") 
-# from 1999 to 2008? Use the base plotting system to make a plot answering this question.
-
-# Generate the graph in the same directory as the source code
-png(filename='~/Exploratory_Data_Analysis/Assignment_2/plot2.png')
-
+# Plot graph
+png(filename='plot2.png')
 barplot(tapply(X=MD$Emissions, INDEX=MD$year, FUN=sum), 
         main='Total Emission in Baltimore City, MD', 
         xlab='Year', ylab=expression('PM'[2.5]))
-
 dev.off()
