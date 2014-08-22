f773686d95d35aa693794e9e1fe99533afd73b8c
Eng Lian Hu <englianhu@users.noreply.github.com>
2014-08-23 01:09:11 +0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,32 +1,29 @@
-# Exploratory Data Analysis - Assignment 2 - Q. #3
-# Ron Mashrouteh May 23, 2014
-
-# Load ggplot2 library
+## Load package
 require(ggplot2)
 
-# Loading provided datasets - loading from local machine
-NEI <- readRDS("~/Exploratory_Data_Analysis/Assignment_2/summarySCC_PM25.rds")
-SCC <- readRDS("~/Exploratory_Data_Analysis/Assignment_2/Source_Classification_Code.rds")
+## Download raw zipped data
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+destfile <- "NEI_data.zip"
+download.file(fileUrl, destfile=paste("data", destfile, sep="/"))
 
-# Sampling
-NEI_sampling <- NEI[sample(nrow(NEI), size=5000, replace=F), ]
+## Unzip the dataset
+unzip(paste("data", destfile, sep="/"), exdir="data")
+data_dir <- setdiff(dir("data"), destfile)
+
+# Read dataset
+NEI <- readRDS("./data/summarySCC_PM25.rds")
+SCC <- readRDS("./data/Source_Classification_Code.rds")
 
 # Baltimore City, Maryland == fips
-MD <- subset(NEI, fips == 24510)
-MD$year <- factor(MD$year, levels=c('1999', '2002', '2005', '2008'))
+BC <- subset(NEI, fips == 24510)
+BC$year <- factor(BC$year, levels=c('1999', '2002', '2005', '2008'))
 
-# Of the four types of sources indicated by the type (point, nonpoint, onroad, nonroad) variable, 
-# which of these four sources have seen decreases in emissions from 1999–2008 for Baltimore City? 
-# Which have seen increases in emissions from 1999–2008? 
-# Use the ggplot2 plotting system to make a plot answer this question.
-
-# Generate the graph in the same directory as the source code
-png(filename='~/Exploratory_Data_Analysis/Assignment_2/plot3.png', width=800, height=500, units='px')
-
-ggplot(data=MD, aes(x=year, y=log(Emissions))) + facet_grid(. ~ type) + guides(fill=F) +
+# Plot graph
+png(filename='plot3.png', width=800, height=500, units='px')
+ggplot(data=BC, aes(x=year, y=log(Emissions))) + facet_grid(. ~ type) + guides(fill=F) +
     geom_boxplot(aes(fill=type)) + stat_boxplot(geom ='errorbar') +
     ylab(expression(paste('Log', ' of PM'[2.5], ' Emissions'))) + xlab('Year') + 
     ggtitle('Emissions per Type in Baltimore City, Maryland') +
     geom_jitter(alpha=0.10)
-
 dev.off()
