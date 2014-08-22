0cd258a21cd397f292b781765feb2c66db7ca274
Eng Lian Hu <englianhu@users.noreply.github.com>
2014-08-23 01:59:47 +0800

    Update plot6.R

--- plot6.R
+++ plot6.R
@@ -1,40 +1,40 @@
-# Exploratory Data Analysis - Assignment 2 - Q. #6
-# Ron Mashrouteh May 23, 2014
+# Load package
+require(ggplot2)
 
-# Load ggplot2 library
-library(ggplot2)
+## Download raw zipped data
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+destfile <- "NEI_data.zip"
+download.file(fileUrl, destfile=paste("data", destfile, sep="/"))
 
-NEI <- readRDS("~/Exploratory_Data_Analysis/Assignment_2/summarySCC_PM25.rds")
-SCC <- readRDS("~/Exploratory_Data_Analysis/Assignment_2/Source_Classification_Code.rds")
+## Unzip the dataset
+unzip(paste("data", destfile, sep="/"), exdir="data")
+data_dir <- setdiff(dir("data"), destfile)
 
+# Read dataset
+NEI <- readRDS("./data/summarySCC_PM25.rds")
+SCC <- readRDS("./data/Source_Classification_Code.rds")
 NEI$year <- factor(NEI$year, levels=c('1999', '2002', '2005', '2008'))
 
-# Baltimore City, Maryland
-# Los Angeles County, California
-MD.onroad <- subset(NEI, fips == '24510' & type == 'ON-ROAD')
+# Subset dataset
+BC.onroad <- subset(NEI, fips == '24510' & type == 'ON-ROAD')
 CA.onroad <- subset(NEI, fips == '06037' & type == 'ON-ROAD')
 
 # Aggregate
-MD.DF <- aggregate(MD.onroad[, 'Emissions'], by=list(MD.onroad$year), sum)
-colnames(MD.DF) <- c('year', 'Emissions')
-MD.DF$City <- paste(rep('MD', 4))
+BC.DF <- aggregate(BC.onroad[, 'Emissions'], by=list(BC.onroad$year), sum)
+colnames(BC.DF) <- c('year', 'Emissions')
+BC.DF$City <- paste(rep('BC', 4))
 
 CA.DF <- aggregate(CA.onroad[, 'Emissions'], by=list(CA.onroad$year), sum)
 colnames(CA.DF) <- c('year', 'Emissions')
 CA.DF$City <- paste(rep('CA', 4))
 
-DF <- as.data.frame(rbind(MD.DF, CA.DF))
+DF <- as.data.frame(rbind(BC.DF, CA.DF))
 
-# Compare emissions from motor vehicle sources in Baltimore City with emissions from motor vehicle sources 
-# in Los Angeles County, California (fips == 06037). Which city has seen greater changes over time 
-# in motor vehicle emissions?
-
-# Generate the graph in the same directory as the source code
-png('~/Exploratory_Data_Analysis/Assignment_2/plot6.png')
-
-ggplot(data=DF, aes(x=year, y=Emissions)) + geom_bar(aes(fill=year)) + guides(fill=F) + 
-    ggtitle('Total Emissions of Motor Vehicle Sources\nLos Angeles County, California vs. Baltimore City, Maryland') + 
-    ylab(expression('PM'[2.5])) + xlab('Year') + theme(legend.position='none') + facet_grid(. ~ City) + 
-    geom_text(aes(label=round(Emissions,0), size=1, hjust=0.5, vjust=-1))
-
+# Plot graph
+png('plot6.png')
+ggplot(data=DF, aes(x=year, y=Emissions)) + geom_bar(stat="identity", aes(fill=year)) + guides(fill=F) +
+       ggtitle('Total Emissions of Motor Vehicle Sources\nLos Angeles County, California vs. Baltimore City, Maryland') + 
+       ylab(expression('PM'[2.5])) + xlab('Year') + theme(legend.position='none') + facet_grid(. ~ City) + 
+       geom_text(aes(label=round(Emissions,0), size=1, hjust=0.5, vjust=-1))
 dev.off()
