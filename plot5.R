961aee344c3c0a5d46a772e7584f97627b1c5e8d
Eng Lian Hu <englianhu@users.noreply.github.com>
2014-08-23 01:52:46 +0800

    Update plot5.R

--- plot5.R
+++ plot5.R
@@ -1,30 +1,32 @@
-# Exploratory Data Analysis - Assignment 2 - Q. #5
-# Ron Mashrouteh May 23, 2014
-
-# Load ggplot2 library
+# Load package
 library(ggplot2)
 
-# Loading provided datasets - loading from local machine
-NEI <- readRDS("~/Exploratory_Data_Analysis/Assignment_2/summarySCC_PM25.rds")
-SCC <- readRDS("~/Exploratory_Data_Analysis/Assignment_2/Source_Classification_Code.rds")
+## Download raw zipped data
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+destfile <- "NEI_data.zip"
+download.file(fileUrl, destfile=paste("data", destfile, sep="/"))
 
+## Unzip the dataset
+unzip(paste("data", destfile, sep="/"), exdir="data")
+data_dir <- setdiff(dir("data"), destfile)
+
+# Read dataset
+NEI <- readRDS("./data/summarySCC_PM25.rds")
+SCC <- readRDS("./data/Source_Classification_Code.rds")
+
+# Subset dataset
 NEI$year <- factor(NEI$year, levels=c('1999', '2002', '2005', '2008'))
-
-# Baltimore City, Maryland == fips
-MD.onroad <- subset(NEI, fips == 24510 & type == 'ON-ROAD')
+BC.onroad <- subset(NEI, fips == 24510 & type == 'ON-ROAD')
 
 # Aggregate
-MD.df <- aggregate(MD.onroad[, 'Emissions'], by=list(MD.onroad$year), sum)
-colnames(MD.df) <- c('year', 'Emissions')
+BC.df <- aggregate(BC.onroad[, 'Emissions'], by=list(BC.onroad$year), sum)
+colnames(BC.df) <- c('year', 'Emissions')
 
-# How have emissions from motor vehicle sources changed from 1999-2008 in Baltimore City? 
-
-# Generate the graph in the same directory as the source code
-png('~/Exploratory_Data_Analysis/Assignment_2/plot5.png')
-
-ggplot(data=MD.df, aes(x=year, y=Emissions)) + geom_bar(aes(fill=year)) + guides(fill=F) + 
-    ggtitle('Total Emissions of Motor Vehicle Sources in Baltimore City, Maryland') + 
-    ylab(expression('PM'[2.5])) + xlab('Year') + theme(legend.position='none') + 
-    geom_text(aes(label=round(Emissions,0), size=1, hjust=0.5, vjust=2))
-
+# Plot graph
+png('plot5.png')
+ggplot(data=BC.df, aes(x=year, y=Emissions)) + geom_bar(stat="identity", aes(fill=year)) +
+       guides(fill=F) + ggtitle('Total Emissions of Motor Vehicle Sources in Baltimore City, Maryland') + 
+       ylab(expression('PM'[2.5])) + xlab('Year') + theme(legend.position='none') + 
+       geom_text(aes(label=round(Emissions,0), size=1, hjust=0.5, vjust=2))
 dev.off()
