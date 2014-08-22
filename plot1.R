a2b490883f059a67e2ff01b8104059ceb99187ba
Eng Lian Hu <englianhu@users.noreply.github.com>
2014-08-22 20:31:47 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,26 +1,24 @@
-# Exploratory Data Analysis - Assignment 2 - Q. #1
-# Ron Mashrouteh May 23, 2014
+## Download raw zipped data
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+destfile <- "household_power_consumption.zip"
+download.file(fileUrl, destfile=paste("data", destfile, sep="/"))
 
-# Loading provided datasets - loading from local machine
-NEI <- readRDS("~/Exploratory_Data_Analysis/Assignment_2/summarySCC_PM25.rds")
-SCC <- readRDS("~/Exploratory_Data_Analysis/Assignment_2/Source_Classification_Code.rds")
+## Unzip the dataset
+unzip(paste("data", destfile, sep="/"), exdir="data")
+data_dir <- setdiff(dir("data"), destfile)
 
-# Sampling
-NEI_sampling <- NEI[sample(nrow(NEI), size=2000, replace=F), ]
+# Read dataset
+NEI <- readRDS("./data/summarySCC_PM25.rds")
+SCC <- readRDS("./data/Source_Classification_Code.rds")
 
 # Aggregate
 Emissions <- aggregate(NEI[, 'Emissions'], by=list(NEI$year), FUN=sum)
 Emissions$PM <- round(Emissions[,2]/1000,2)
 
-# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
-# Using the base plotting system, make a plot showing the total PM2.5 emission from all sources 
-# for each of the years 1999, 2002, 2005, and 2008.
-
-# Generate the graph in the same directory as the source code
-png(filename='~/Exploratory_Data_Analysis/Assignment_2/plot1.png')
-
+# Plot graph and save as image file
+png(filename='plot1.png')
 barplot(Emissions$PM, names.arg=Emissions$Group.1, 
         main=expression('Total Emission of PM'[2.5]),
         xlab='Year', ylab=expression(paste('PM', ''[2.5], ' in Kilotons')))
-
 dev.off()
